6cc2a1e1bc7f06e2ded8a9fa48ba0ae4f4c7e466
Arthur Sobrinho <arthur.sobrinho2014@gmail.com>
2022-08-26 10:21:38 -0300

    Innitial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/PF/src
+# Build directory: /root/PF/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("driver/nemo_simulator")
