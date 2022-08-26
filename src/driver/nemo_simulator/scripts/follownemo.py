9e737f8065bf53adddc59c6be69f23db9f755a3c
Arthur Sobrinho <arthur.sobrinho2014@gmail.com>
2022-08-26 14:35:16 -0300

    updating follownemo.py

--- src/driver/nemo_simulator/scripts/follownemo.py
+++ src/driver/nemo_simulator/scripts/follownemo.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 import rospy
 import geometry_msgs.msg as gm
-import std_msgs.msg
-import numpy 
 
-def callback(msg):
+
+def callback(msg: gm.PointStamped):
+    pub = rospy.Publisher("/cmd_vel", gm.Twist, queue_size=10)
+
+    vel = gm.Twist()
+    
+
+    vel.linear.x = -msg.point.x
+    vel.linear.y = -msg.point.y
+    vel.linear.z = -msg.point.z
+
+    vel.angular.x = 0
+    vel.angular.y = 0
+    vel.angular.z = 0
+   
+    pub.publish(vel)
+
+
     
 
 if __name__ == '__main__':
-    rospy.Subscriber("/sonar_data", gm.PointStamped, callback)
-    
+    rospy.init_node("follownemo", anonymous=True)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        rospy.sleep()       
+        rospy.Subscriber("/sonar_data", gm.PointStamped, callback)
+        
+
+        
